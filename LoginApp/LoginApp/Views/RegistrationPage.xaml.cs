e0ec34e078748dce9416720809c45ec5349b9ca7
Mohamed Bakayoko <Mohamed Bakayoko@MOMOBAK>
2022-12-06 22:24:44 -0500

    Add project files.

---
+++ LoginApp/LoginApp/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using LoginApp.Tables;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void Button_Clicked(object sender, System.EventArgs e)
+        {
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDatabase.db");
+            var db = new SQLiteConnection(dbpath);
+            db.CreateTable<RegUserTable>();
+
+            var item = new RegUserTable()
+            {
+                UserName = EntryUserName.Text,
+                Password = EntryUserPassword.Text,
+                Email = EntryUserEmail.Text,
+                PhoneNumber = EntryUserPhoneNumber.Text
+            };
+            db.Insert(item);
+            Device.BeginInvokeOnMainThread(async() =>
+            {
+
+                var result = await this.DisplayAlert("Congratulation", "UserRegistrationSuccesfull", "Yes", "Cancel");
+                if (result)
+                    await Navigation.PushAsync(new LoginPage());
+            });
+        }
+    }
+}
